e20228b2068e32bbca001d04eecbbadd57af3a9e
Yenifer ochoa <yenifere.ochoa@gmail.com>
2019-02-17 17:09:53 -0500

    agregue el usuario en el allowed_hosts en settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8&rd_u%tm&&5-qt(!mv16se($ja9*_5(hyw-u0pv&vz48e5mt4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<tu-usuario>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'yenifermarquez.pythonanywhere.com']
 
 
 # Application definition
